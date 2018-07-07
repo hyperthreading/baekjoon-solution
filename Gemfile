455fba17d28a1e5e91c56e9ae1e2d794a78b5e03
hyperthreading <no1expman@gmail.com>
2018-07-07 16:45:25 +0900

    Solved 2011

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "pry", "~> 0.11.3"
+
+gem "pry-doc", "~> 0.13.4"
+
+gem "method_source", "~> 0.9.0"
--- dynamic-programming/2011.rb
+++ dynamic-programming/2011.rb
@@ -1,7 +1,11 @@
-module PassCode
-  module_function :call
-
-  def call
-    
-  end
+def possible_combination_from(ns, offset_start = 0, cache = {})
+  cache[ns.length] = cache[ns.length - 1] = 1
+  (offset_start..ns.length - 1).reverse_each { |offset|
+    cache[offset] = 0 and next if ns[offset] == '0'
+    next if cache[offset]
+    cache[offset] = cache[offset + 1]
+    cache[offset] += cache[offset + 2] unless ns.slice(offset, 2).to_i > 26
+  }
+  cache[offset_start]
 end
+puts possible_combination_from(gets.chomp) % 1000000
